07ccc76780ab6216222c01b16d4e57db4fb6faaf
Sarvesh Doshi <sarvesh.doshi@in10media.com>
2025-04-06 12:03:42 +0530

    Initial Commit

---
+++ LiveActivityUIKIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveActivityUIKIt
+//
+//  Created by Sarvesh Doshi on 06/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
